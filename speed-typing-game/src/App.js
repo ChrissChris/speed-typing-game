4abce7613f6fb30330ef25cabbd89aae79d5ed84
Chriss Chris <zenskech@outlook.com>
2020-12-01 13:19:15 +0500

    edit

--- speed-typing-game/src/App.js
+++ speed-typing-game/src/App.js
@@ -2,13 +2,36 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const [text, setText] = useState("");
-  return (
-    <div className="App">
-      <h1>How fast do you type?</h1>
-      <textarea></textarea>
+  const [timeRemaining, setTimeRemaining] = useState(5);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
 
-      <button>Start</button>
-      <h4>Time Remaining:</h4>
+  function handleChange(e) {
+    const { value } = e.target;
+    setText(value);
+  }
+
+  function calculateWordCount(text) {
+    const wordsArr = text.trim().split(" ");
+    return wordsArr.filter((word) => word !== "").length;
+  }
+
+  useEffect(() => {
+    if (isTimeRunning && timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining((time) => time - 1);
+      }, 1000);
+    }
+  }, [timeRemaining]);
+
+  return (
+    <div>
+      <h1>How fast do you type?</h1>
+      <textarea onChange={handleChange} value={text} />
+      <h4>Time remaining: {timeRemaining}</h4>
+      <button onClick={() => console.log(calculateWordCount(text))}>
+        Start
+      </button>
+      <h1>Word count: ???</h1>
     </div>
   );
 }
